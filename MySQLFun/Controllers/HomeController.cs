390084442145a136ce6465c4bede1d20826b9139
Layton Barney <laytonb2@byu.edu>
2022-03-28 16:43:14 -0600

    Add project files.

---
+++ MySQLFun/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySQLFun.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBowlersRepository _repo { get; set; }
+        public HomeController(IBowlersRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _repo.Bowlers.ToList();
+            return View(blah);
+        }
+    }
+}
